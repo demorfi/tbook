cd250e30ed6ae4f075ff0400859e95fef72eb11d
demorfi <demorfi@gmail.com>
2019-01-31 01:09:04 +0700

    create database structure

---
+++ database/migrations/2019_01_30_131319_create_category_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'category_products',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('category_id');
+                $table->unsignedInteger('product_id');
+                $table->unique(['category_id', 'product_id']);
+                $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+                $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_products');
+    }
+}
